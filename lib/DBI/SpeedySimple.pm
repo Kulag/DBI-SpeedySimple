ec5a9277b315addf688dc07f1ad0430ce66ef348
Kulag <g.kulag@gmail.com>
2012-10-04 18:14:57 +0000

    Remove unused Exporter boilerplate

--- lib/DBI/SpeedySimple.pm
+++ lib/DBI/SpeedySimple.pm
@@ -6,29 +6,8 @@ use warnings;
 
 require DBI;
 use Encode;
-require Exporter;
 
-our @ISA = qw(Exporter);
-
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use DBI::SpeedySimple ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = ( 'all' => [ qw(
-	
-) ] );
-
-our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
-
-our @EXPORT = qw(
-	
-);
-
-our $VERSION = '0.02';
-
+our $VERSION = '0.03';
 
 sub new {
 	my $self = bless {}, shift;
